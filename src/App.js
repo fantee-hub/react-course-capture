5b58805771f0c8f18dcbe233f237d1e293b14f07
Tobi Faniran <tobifaniran@gmail.com>
2021-01-15 23:38:24 +0100

    8. Faq styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyling from "./components/GlobalStyling";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
       <GlobalStyling />
+      <Nav />
       <AboutUs />
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,55 @@
+import React from "react";
+import styled from "styled-components";
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <a href="#" id="logo">
+          Capture
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="#">1. About Us</a>
+        </li>
+        <li>
+          <a href="#">2. Our Work</a>
+        </li>
+        <li>
+          <a href="#">3. Contact Us</a>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 10vh;
+  padding: 1rem 10rem;
+  background: #282828;
+  a {
+    color: white;
+    text-decoration: none;
+    li {
+      padding-left: 100rem;
+    }
+  }
+  ul {
+    list-style: none;
+    display: flex;
+  }
+  #logo {
+    font-family: Lobster, cursive;
+    font-size: 1.5rem;
+    font-weight: lighter;
+  }
+  li {
+    padding-left: 100rem;
+  }
+`;
+
+export default Nav;
