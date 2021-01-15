5ad3eb55524f69da4026d0d747cb4f0b74e1481a
Tobi Faniran <tobifaniran@gmail.com>
2021-01-15 21:53:02 +0100

    7. Faq section styling

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import { About } from "../styled";
+import styled from "styled-components";
 const FaqSection = () => {
   return (
-    <div className="faq">
+    <Faq>
       <h2>
         Any Questions <span>FAQ</span>
       </h2>
@@ -15,6 +16,7 @@ const FaqSection = () => {
             quidem.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="questions">
         <h4>Daily Schedule</h4>
@@ -25,6 +27,7 @@ const FaqSection = () => {
             quidem.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="questions">
         <h4>Different Payment Methods</h4>
@@ -35,6 +38,7 @@ const FaqSection = () => {
             quidem.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
       <div className="questions">
         <h4>What Product Do You Offer?</h4>
@@ -45,8 +49,36 @@ const FaqSection = () => {
             quidem.
           </p>
         </div>
+        <div className="faq-line"></div>
       </div>
-    </div>
+    </Faq>
   );
 };
+
+const Faq = styled(About)`
+  display: block;
+  span {
+    display: block;
+  }
+  h2 {
+    padding-bottom: 2rem;
+    font-weight: lighter;
+  }
+  .faq-line {
+    background: #cccccc;
+    height: 0.2rem;
+    width: 100%;
+    margin: 2rem 0rem;
+  }
+  .questions {
+    padding: 3rem 0rem;
+    cursor: pointer;
+  }
+  .answer {
+    padding: 2rem 0rem;
+    p {
+      padding: 1rem 0rem;
+    }
+  }
+`;
 export default FaqSection;
